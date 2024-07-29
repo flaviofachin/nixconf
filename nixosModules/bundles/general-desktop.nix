4c9fc130cc4c5c4acfb2e6b7a8e1f8b717974cac
flaviofachin <flaviofachin75@gmail.com>
2024-07-29 22:08:39 +0300

    Update general-desktop.nix
    Israel time zone default

--- nixosModules/bundles/general-desktop.nix
+++ nixosModules/bundles/general-desktop.nix
@@ -9,22 +9,8 @@
   myNixOS.system-controller.enable = lib.mkDefault false;
   myNixOS.virtualisation.enable = lib.mkDefault true;
   myNixOS.stylix.enable = lib.mkDefault true;
-
-  # Central European time zone
-  time.timeZone = "Europe/Bratislava";
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "sk_SK.UTF-8";
-    LC_IDENTIFICATION = "sk_SK.UTF-8";
-    LC_MEASUREMENT = "sk_SK.UTF-8";
-    LC_MONETARY = "sk_SK.UTF-8";
-    LC_NAME = "sk_SK.UTF-8";
-    LC_NUMERIC = "sk_SK.UTF-8";
-    LC_PAPER = "sk_SK.UTF-8";
-    LC_TELEPHONE = "sk_SK.UTF-8";
-    LC_TIME = "sk_SK.UTF-8";
-  };
-
+  myNixos.Israel-timezone.enable = lib.mkDefault true;
+  
   # Enable sound with pipewire.
   sound.enable = true;
   security.rtkit.enable = true;

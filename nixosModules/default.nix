3d12ae95805e5b89b712e42dcd9ee052b5ef4338
flaviofachin <flaviofachin75@gmail.com>
2024-07-29 18:48:01 +0300

    Update default.nix
    Changed according to mylib

--- nixosModules/default.nix
+++ nixosModules/default.nix
@@ -9,49 +9,16 @@
 }: let
   cfg = config.myNixOS;
 
-  # Taking all modules in ./features and adding enables to them
-  features =
-    myLib.extendModules
-    (name: {
-      extraOptions = {
-        myNixOS.${name}.enable = lib.mkEnableOption "enable my ${name} configuration";
-      };
-
-      configExtension = config: (lib.mkIf cfg.${name}.enable config);
-    })
-    (myLib.filesIn ./features);
-
-  # Taking all module bundles in ./bundles and adding bundle.enables to them
-  bundles =
-    myLib.extendModules
-    (name: {
-      extraOptions = {
-        myNixOS.bundles.${name}.enable = lib.mkEnableOption "enable ${name} module bundle";
-      };
-
-      configExtension = config: (lib.mkIf cfg.bundles.${name}.enable config);
-    })
-    (myLib.filesIn ./bundles);
-
-  # Taking all module services in ./services and adding services.enables to them
-  services =
-    myLib.extendModules
-    (name: {
-      extraOptions = {
-        myNixOS.services.${name}.enable = lib.mkEnableOption "enable ${name} service";
-      };
-
-      configExtension = config: (lib.mkIf cfg.services.${name}.enable config);
-    })
-    (myLib.filesIn ./services);
+  # Taking all modules in the directories specified and adding enables to them
+  magDirs = mylib.magicDirs ["features" "bundles" "services"];
 in {
   imports =
     [
       inputs.home-manager.nixosModules.home-manager
     ]
-    ++ features
-    ++ bundles
-    ++ services;
+    ++ magDirs.features
+    ++ magDirs.bundles
+    ++ magDirs.services;
 
   options.myNixOS = {
     hyprland.enable = lib.mkEnableOption "enable hyprland";
